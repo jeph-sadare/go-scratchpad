cb6688d48d9fa47bce93d360c244653427bd07f0
Jephthah Sadare <jephsadare@gmail.com>
2026-02-11 21:44:32 +0000

    refactored for empty string

--- hello_test.go
+++ hello_test.go
@@ -2,10 +2,22 @@ package main
 
 import "testing"
 
-func TestHello(t *testing.T) {
-	got := Hello("Jephthah")
-	wanted := "Hello Jephthah"
-	if got != wanted {
-		t.Errorf("got %q wanted %q", got, wanted)
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q wanted %q", got, want)
 	}
+}
+
+func TestHello(t *testing.T) {
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Jephthah")
+		want := "Hello Jephthah"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("say 'Hello world', when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello world"
+		assertCorrectMessage(t, got, want)
+	})
 }
